8743923bd7ec78a72003c17918ffdefa5a54ca1a
Lóan Burger <loan.burger@gmail.com>
2021-11-15 20:36:06 +1300

    Change Mock implementation

--- project/src/api/authenticate.strava.api.test.ts
+++ project/src/api/authenticate.strava.api.test.ts
@@ -1,9 +1,13 @@
 import { IAuthenticateStravaResponse } from 'src/models';
 import MockAdapter from 'axios-mock-adapter';
 import { authenticateWithStrava } from 'src/api';
-import { axiosApi } from 'src/utils';
-import * as storageUtils from 'src/utils/storage.util';
+import { axiosApi, getObject } from 'src/utils';
 import { mockStravaSession } from 'src/test/utils';
+
+jest.mock('src/utils/storage.util', () => ({
+  getObject: jest.fn(),
+  setItem: jest.fn(),
+}));
 
 describe(`[api] ${authenticateWithStrava.name}`, () => {
   const mock: MockAdapter = new MockAdapter(axiosApi);
@@ -11,6 +15,18 @@ describe(`[api] ${authenticateWithStrava.name}`, () => {
   describe('When the grant type is authorization_code', () => {
     const mockCode = 'abcdef';
     const stravaEndPoint = `https://www.strava.com/oauth/token?client_id=&client_secret=&grant_type=authorization_code&code=${mockCode}`;
+
+    beforeEach(() => {
+      const mockGetObject = getObject as jest.MockedFunction<typeof getObject>;
+      mockGetObject.mockImplementation((key) => {
+        if (key === 'strava_session') return mockStravaSession;
+        return undefined;
+      });
+    });
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
 
     it('should return a successful response when the call suicceeds', async () => {
       mock.onPost(stravaEndPoint).reply(200, mockStravaSession);
@@ -37,13 +53,18 @@ describe(`[api] ${authenticateWithStrava.name}`, () => {
 
   describe('When the grant type is refresh_token', () => {
     const stravaEndPoint = `https://www.strava.com/oauth/token?client_id=&client_secret=&grant_type=refresh_token&refresh_token=mock-refresh-token`;
+    const mockGetObject = getObject as jest.MockedFunction<typeof getObject>;
 
     afterEach(() => {
       jest.clearAllMocks();
     });
 
     it('should return a successful response when the api call succeeds', async () => {
-      jest.spyOn(storageUtils, 'getObject').mockReturnValue(mockStravaSession);
+      mockGetObject.mockImplementation((key) => {
+        if (key === 'strava_session') return mockStravaSession;
+        return undefined;
+      });
+
       mock.onPost(stravaEndPoint).reply(200, mockStravaSession);
       await authenticateWithStrava().then(
         (res: IAuthenticateStravaResponse) => {
@@ -55,7 +76,7 @@ describe(`[api] ${authenticateWithStrava.name}`, () => {
     });
 
     it('should return a failed response when the api call fails', async () => {
-      jest.spyOn(storageUtils, 'getObject').mockReturnValue(undefined);
+      mockGetObject.mockImplementation(undefined);
       mock.onPost(stravaEndPoint).reply(503);
       await authenticateWithStrava().then(
         (res: IAuthenticateStravaResponse) => {
